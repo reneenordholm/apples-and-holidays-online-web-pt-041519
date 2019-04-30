f36dc5c4bd3d8350fad1eeb4ffb3cc99a7a7f1b0
Renee Nordholm <reneenordholm@gmail.com>
2019-04-30 01:40:57 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,8 +58,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holiday|
     holiday.each do |supplies|
-      puts "#{season}:"
-      puts "#{holiday}: #{supplies}"
+      puts "#{season}: #{holiday}: #{supplies}"
   end
  end
 end
