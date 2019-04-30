68418469cd4bb7795ebc3e29d2469f341a27949c
Renee Nordholm <reneenordholm@gmail.com>
2019-04-30 01:38:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,8 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
-    holiday.each 
+    holiday.each do |supplies|
+      puts 
 end
 
 def all_holidays_with_bbq(holiday_hash)
